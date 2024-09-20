52d6a17cbfd74b97a857ae624e857502b576a4a2
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2024-09-21 02:37:24 +0600

    Impliment Tokenization

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
-# Home Repair Services Backend
+# Home Repair Services Backend (RENOXY)
 
 Welcome to the **Home Repair Services Backend**, built to support a platform where users can offer, book, and manage various home repair services like plumbing, electrical repairs, and carpentry. This backend is developed using **Node.js** and **Express.js**, with **MongoDB** as the database and **JWT (JSON Web Token)** for secure api and authentication.
 
 ## Frontend Repository
 [Click Here to Visit Frontend Code](https://github.com/dear-mahmud-bd/home-repair-services-client)
 
+## Features
+- **JWT Authentication**: Secure access to protected routes using JSON Web Tokens.
+- **Role-Based Access**: Differentiate between service providers and customers.
+- **CRUD Operations**: Create, read, update, and delete services.
+- **Booking System**: Customers can book services, and service providers can manage them.
+- **MongoDB Integration**: Data persistence using MongoDB.
+- **Middleware**: Custom middleware for logging and authentication.
+- **Cookie-Based Token Management**: Use cookies for storing and handling JWT tokens.
+
+## Technologies Used
+- **Node.js**: Backend runtime environment for running JavaScript on the server.
+- **Express.js**: Web framework for building RESTful APIs quickly and efficiently.
+- **MongoDB**: NoSQL database used to store user, service, and booking information.
+- **JWT (JSON Web Token)**: Used for secure user authentication and session management.
+- **CORS (Cross-Origin Resource Sharing)**: Handles cross-origin requests to allow the frontend to communicate with the backend.
+- **Cookie-Parser**: Middleware for parsing cookies, used for managing authentication tokens securely in HTTP-only cookies.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
@@ -12,11 +13,14 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({
     origin: [
-        'http://localhost:5173',
+        // 'http://localhost:5173',
+        'https://home-repair-service-03.web.app',
+        'https://home-repair-service-03.firebaseapp.com',
     ],
     credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -31,6 +35,44 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+
+
+
+// middlewares 
+const logger = (req, res, next) => {
+    console.log('log: info-> ', req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('verifyToken: ', token);
+    // no token available 
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+    // next();
+}
+
+// const cookieOption = {
+//     httpOnly: true,
+//     secure: true,
+//     sameSite: 'none'
+// };
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -47,7 +89,7 @@ async function run() {
 
 
 
-        
+
         // auth related api...
         // create a token ...
         app.post('/jwt', async (req, res) => {
@@ -56,38 +98,29 @@ async function run() {
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 
             // res.send({ token });
-            res.cookie('token', token, {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'none'
-            }).send({ success: true });
+            res.cookie('token', token, cookieOptions).send({ success: true });
         })
 
         // remove token when log-out...
         app.post('/logout', async (req, res) => {
             const user = req.body;
             // console.log('logging out', user);
-            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+            res.clearCookie('token', { ...cookieOptions, maxAge: 0 }).send({ success: true })
         })
 
 
 
 
 
-
-
-
-
-
         // Get most popular services...
-        app.get('/popular-services', async (req, res) => {
+        app.get('/popular-services', logger, async (req, res) => {
             const cursor = serviceCollection.find().sort({ serviceTotalOrder: -1 }).limit(4);
             const result = await cursor.toArray();
             res.send(result);
         });
 
         // Get all services ...
-        app.get('/services', async (req, res) => {
+        app.get('/services', logger, async (req, res) => {
             const serviceName = req.query.service_name;
             let query = {};
             if (serviceName) {
@@ -99,8 +132,11 @@ async function run() {
         })
 
         // Get specific user services ...
-        app.get('/user-services', async (req, res) => {
+        app.get('/user-services', logger, verifyToken, async (req, res) => {
             const userEmail = req.query.user_email;
+            if (req.user.email !== userEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const query = { serviceProviderEmail: userEmail };
             const cursor = serviceCollection.find(query);
             const result = await cursor.toArray();
@@ -108,7 +144,7 @@ async function run() {
         });
 
         // Get a specific service ...
-        app.get('/services/:_id', async (req, res) => {
+        app.get('/services/:_id', logger, async (req, res) => {
             const id = req.params._id;
             if (!ObjectId.isValid(id)) {
                 return res.status(400).send({ message: 'Invalid ID format. ID must be a 24-character hexadecimal string.' });
@@ -119,7 +155,7 @@ async function run() {
         })
 
         // Add a specific service in database ...
-        app.post('/services', async (req, res) => {
+        app.post('/services', logger, verifyToken, async (req, res) => {
             const newService = req.body;
             // console.log(newService);
             const result = await serviceCollection.insertOne(newService);
@@ -127,7 +163,7 @@ async function run() {
         })
 
         // Update a service from specific user
-        app.put('/services/:_id', async (req, res) => {
+        app.put('/services/:_id', logger, verifyToken, async (req, res) => {
             const id = req.params._id;
             const filter = { _id: new ObjectId(id) };
             const options = { upsert: true };
@@ -151,9 +187,9 @@ async function run() {
         })
 
         // Delete specific user service ...
-        app.delete('/services/:_id', async (req, res) => {
+        app.delete('/services/:_id', logger, verifyToken, async (req, res) => {
             const id = req.params._id;
-            console.log(id);
+            // console.log(id);
             const query = { _id: new ObjectId(id) }
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
@@ -161,17 +197,24 @@ async function run() {
 
 
 
+
+
         // Add a specific service in database ...
-        app.post('/bookings', async (req, res) => {
+        app.post('/bookings', logger, verifyToken, async (req, res) => {
             const newBooking = req.body;
-            console.log(newBooking);
+            // console.log(newBooking);
             const result = await bookingCollection.insertOne(newBooking);
             res.send(result);
         })
 
         // Get all bookings for service holder ...
-        app.get('/holder-bookings', async (req, res) => {
+        app.get('/holder-bookings', logger, verifyToken, async (req, res) => {
+            // console.log("Cookie paysi naki: ", req.cookies);
             const userEmail = req.query.user_email;
+            // console.log('token owner info: ', req.user);
+            if (req.user.email !== userEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const query = { serviceHolderEmail: userEmail };
             const cursor = bookingCollection.find(query);
             const result = await cursor.toArray();
@@ -179,8 +222,11 @@ async function run() {
         });
 
         // Get all bookings for service provider ...
-        app.get('/provider-bookings', async (req, res) => {
+        app.get('/provider-bookings', logger, verifyToken, async (req, res) => {
             const userEmail = req.query.user_email;
+            if (req.user.email !== userEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const query = { serviceProviderEmail: userEmail };
             const cursor = bookingCollection.find(query);
             const result = await cursor.toArray();
@@ -188,7 +234,7 @@ async function run() {
         });
 
         // Update booking status with service provider ...
-        app.patch('/bookings-status/:_id', async (req, res) => {
+        app.patch('/bookings-status/:_id', logger, verifyToken, async (req, res) => {
             const id = req.params._id;
             // console.log(id);
             const filter = { _id: new ObjectId(id) };
@@ -199,10 +245,11 @@ async function run() {
                     status: updatedStatus.status,
                 }
             };
-            console.log(updatedStatus.status, setStatus);
+            // console.log(updatedStatus.status, setStatus);
             const result = await bookingCollection.updateOne(filter, setStatus, options);
             res.send(result);
         });
+
 
 
 
